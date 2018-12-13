21d32b8743a67cdede726a113c5cb44b6e9dba86
Janelle Erickson-Goettl <janelle.erickson.goettl@gmail.com>
2018-12-13 18:21:34 -0500

    finally figured out axios calls

--- main.js
+++ main.js
@@ -1,20 +1,54 @@
 "use strict";
 
+// hp - integerx
+// attack - integer
+// defense - integer
+// abilities - array of strings
+
+// Archeops: https://pokeapi.co/api/v2/pokemon/567/
+// Armaldo: https://pokeapi.co/api/v2/pokemon/348/
+// Rampardos: https://pokeapi.co/api/v2/pokemon/409/
+
+// let data = response.data;
+
+// let ul = document.createElement(ul);
+// let li = document.createElement(li);
+// let p = document.createElement(p);
+
+
+class Pokemon {
+  constructor(hp, attack, defense, abilities) {
+    this.hp = hp;
+    this.attack = attack;
+    this.defense = defense;
+    this.abilities = abilities[i];
+  }
+}
+
+
+
+let stats = new Pokemon();
 
 // call for Archeops
 axios.get("https://pokeapi.co/api/v2/pokemon/567/")
   .then(response => {
-    
-  })
+    let data = response.data;
+    let hp = data.stats[5];
+    let attack = data.stats[4];
+    let defense = data.stats[];
 
-// call for Armaldo
-axios.get("https://pokeapi.co/api/v2/pokemon/348/")
-  .then(response => {
-    
-  })
-
-  // call for Rampardos
-axios.get("https://pokeapi.co/api/v2/pokemon/409/")
-.then(response => {
+    console.log(response.data);
   
-})
+  })
+
+// // call for Armaldo
+// axios.get("https://pokeapi.co/api/v2/pokemon/348/")
+//   .then(response => {
+    
+//   })
+
+//   // call for Rampardos
+// axios.get("https://pokeapi.co/api/v2/pokemon/409/")
+// .then(response => {
+  
+// })
