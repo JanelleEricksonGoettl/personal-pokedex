de32434f506492bcf2a163ce687fdbe99119c914
Janelle Erickson-Goettl <janelle.erickson.goettl@gmail.com>
2018-12-13 12:42:57 -0500

    fixed bootstrap column breakpoints for my display and added bg img overlay

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+// call for Archeops
+axios.get("https://pokeapi.co/api/v2/pokemon/567/")
+  .then(response => {
+    
+  })
+
+// call for Armaldo
+axios.get("https://pokeapi.co/api/v2/pokemon/348/")
+  .then(response => {
+    
+  })
+
+  // call for Rampardos
+axios.get("https://pokeapi.co/api/v2/pokemon/409/")
+.then(response => {
+  
+})
--- rampardos.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-axios.get("https://pokeapi.co/api/v2/pokemon/409/")
-  .then(response => {
-    
-  })
